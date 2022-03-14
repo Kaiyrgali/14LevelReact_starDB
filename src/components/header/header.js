42b9961cf1f6c0f81e61502e3c3344f204bc298f
Kaiyrgali Bekkali <kairgalib@ya.ru>
2022-03-14 19:04:56 +0500

    before create secretPage

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,7 +11,7 @@ const Header = ({ onServiceChange }) => {
       </h3>
       <ul className="d-flex">
         <li>
-          <Link to = 'people'>People</Link> 
+          <Link to = 'people/'>People</Link> 
         </li>
         <li>
           <Link to = 'planets'>Planets</Link>
--- src/components/pages/people-page.js
+++ src/components/pages/people-page.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PersonDetails, PersonList } from '../sw-components';
 import Row from '../row';
+import { useNavigate, useParams } from 'react-router-dom';
 
-export default class PeoplePage extends Component {
+const PeoplePage =() => {
 
-  state = {
-    selectedItem: null
-  };
-
-  onItemSelected = (selectedItem) => {
-    this.setState({ selectedItem });
-  };
-
-  render() {
-    const { selectedItem } = this.state;
-
+  const params = useParams();
+  const prodId = params.id;
+  const navigate = useNavigate();
+  
     return (
       <Row
-        left={<PersonList onItemSelected={this.onItemSelected} />}
-        right={<PersonDetails itemId={selectedItem} />} />
+        left={<PersonList onItemSelected={(e) => navigate(`/people/${e}`)} />}
+        right={<PersonDetails  itemId = {prodId} />} />
     );
   }
 
-}
+export default PeoplePage;
